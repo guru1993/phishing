93c0182325d851295cd180b0d5add3a89160d95f
guru l <38384331+guru1993@users.noreply.github.com>
2020-04-21 11:44:31 +0530

    Update wishfish.sh

--- wishfish.sh
+++ wishfish.sh
@@ -13,8 +13,7 @@ echo -e "\033[31m   \ \/  \/ / | / __| '_  |\e[0m|  __| | / __| '_ \      ";
 echo -e "\033[31m    \  /\  /  | \__ \ | | |\e[0m| |    | \__ \ | | |     ";
 echo -e "\033[31m     \/  \/   |_|___/_| |_|\e[0m|_|    |_|___/_| |_| v2.0";
 
-printf "\e[1;77m Website :- www.hackersking.in \e[0m \n"
-printf "\e[1;77m v2.0 Coded By github.com/kinghacker0 \e[0m \n"
+printf "\e[1;77m v2.0 Coded By https://github.com/guru1993/phishing.git \e[0m \n"
 
 }
 
